1e2fc8f9d2712e2f05f93a34df907cbee2d0c4d8
GitFornieles <alefornieles@hotmail.com>
2022-07-10 14:49:22 -0300

    JSIII Homework

--- 03-JS-II/homework/prueba.js
+++ 03-JS-II/homework/prueba.js
@@ -1,14 +1,20 @@
-function doWhile(numero) {
-  //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
-  //Retornar el valor final.
-  //Usar el bucle do ... while.
-  let i=0
+function continueStatement(numero) {
+  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
+  //Guardar cada nuevo valor en un array.    
+  //Devolver el array
+  //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
+  //Pista: usá el statement 'continue'
+  // Tu código:
+  let c=0;
+  let array=[];
   do{
-    i=i+1;
-    numero = numero + 5;
-  }while(i<8);
-  return numero;
+    c++;
+    if(c===5){continue;}
+    array.push(numero=numero+2);
+  }while(c<10);
+  return array;
 }
-console.log("5 da " + doWhile(5));
-console.log("100 da " + doWhile(100));
-console.log("-1 da " + doWhile(-1));
+
+console.log(continueStatement(50));
+
+
